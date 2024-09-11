523a634fdddcd4f29650a1cc44ab644b112976f1
Anton Kuznetsov <xxxasmodayxxx@gmail.com>
2024-09-11 20:44:26 +0500

    edit lawn grass

--- src/lawn_grass.py
+++ src/lawn_grass.py
@@ -9,21 +9,21 @@ class LawnGrass(Product):
         self.color = color
 
 
-if __name__ == "__main__":
-    grass1 = LawnGrass.new_product({
-        "name": "Lawngrass",
-        "description": "Lawngrass small size",
-        "price": 1500.0,
-        "quantity": 5,
-        "country": "Russia",
-        "germination_period": "2 month",
-        "color": "Зелёный"
-    })
-
-    print(grass1.name)
-    print(grass1.description)
-    print(grass1.price)
-    print(grass1.quantity)
-    print(grass1.country)
-    print(grass1.germination_period)
-    print(grass1.color)
+# if __name__ == "__main__":
+#     grass1 = LawnGrass.new_product({
+#         "name": "Lawngrass",
+#         "description": "Lawngrass small size",
+#         "price": 1500.0,
+#         "quantity": 5,
+#         "country": "Russia",
+#         "germination_period": "2 month",
+#         "color": "Зелёный"
+#     })
+#
+#     print(grass1.name)
+#     print(grass1.description)
+#     print(grass1.price)
+#     print(grass1.quantity)
+#     print(grass1.country)
+#     print(grass1.germination_period)
+#     print(grass1.color)
